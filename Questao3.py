67fff30bffa8cee2697d90bf419b8f8268109da3
Beatriz Maia <prbeatrizmaia@gmail.com>
2023-02-28 03:15:56 -0300

    final

--- Questao3.py
+++ Questao3.py
@@ -1,9 +1,25 @@
-def print_hi(name):
-    # Função
-    print(f'Hi, {name}')
+import json
 
+with open ("dados.json", encoding='utf-8') as info_json:
+    dados = json.load(info_json)
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def menorvalor(dados):
+    menor_valor = min(dados, key=lambda d : d['valor'])['valor']
+    print('\nMenor valor:',menor_valor,'\nDia(s):')
+    return [item['dia'] for item in dados if item['valor'] == menor_valor]
+def maiorvalor(dados):
+    maior_valor = max(dados, key=lambda d: d['valor'])['valor']
+    print('\nMaior valor:',maior_valor,'\nDia(s):')
+    return [item['dia'] for item in dados if item['valor'] == maior_valor]
+def faturamento(dados):
+   print('\nNúmero de dias no mês em que o valor de faturamento diário foi superior à média mensal:')
+   valores = []
+   for i in dados:
+       if i['valor'] != 0:
+           valores.append(i['valor'])
+   media_mensal = sum(valores)/len(valores)
+   return [item['dia'] for item in dados if item['valor'] > media_mensal]
 
-
+print(maiorvalor(dados))
+print(menorvalor(dados))
+print(faturamento(dados))
--- Questao4.py
+++ Questao4.py
@@ -1,9 +1,17 @@
+faturamento_mensal = {
+    'SP': 67836.43,
+    'RJ': 36678.66,
+    'MG': 29229.88,
+    'ES': 27165.48,
+    'Outros': 19849.53
+}
+total= sum(faturamento_mensal.values())
+percentual = dict()
+for item in faturamento_mensal:
+    percentual[item] = '{:.2%}'.format(faturamento_mensal[item] / total)
 
-def print_hi(name):
-    # Função
-    print(f'Hi, {name}')
-
-
-print_hi('PyCharm')
+print("Faturamento Mensal(Por estado) = ", faturamento_mensal)
+print("Faturamento Total mensal = ", total)
+print("Faturamento Percentual(Por estado) = ", percentual)
 
 
